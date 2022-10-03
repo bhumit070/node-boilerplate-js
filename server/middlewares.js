b24e4de89fbb20a10bfae9c8ceed6520c16b6869
bhumit070 <ganatrabhoomit070@gmail.com>
2022-10-03 22:32:55 +0530

    updated app middlewares file and eslint rule make camel case mandatory

--- server/middlewares.js
+++ server/middlewares.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-function register_app_middlewares(app) {
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
-	app.use(cors());
-	app.use(morgan('dev'));
+function registerAppMiddlewares(app) {
+	app
+		.use(express.json())
+		.use(express.urlencoded({ extended: true }))
+		.use(cors())
+		.use(morgan('dev'));
 }
 
-module.exports = register_app_middlewares;
+module.exports = registerAppMiddlewares;
 
