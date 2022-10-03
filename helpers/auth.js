e5f7d62ecd091d36a1b1a807aaad54f253b37619
bhumit070 <ganatrabhoomit070@gmail.com>
2022-10-03 22:12:27 +0530

    change helpers structure

---
+++ helpers/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const constants = require('../config/constants');
+
+function encodeToken(user) {
+	const token = jwt.sign(user, constants.JWT_SECRET, {
+		expiresIn: constants.JWT_EXPIRES_IN
+	});
+	return token;
+}
+
+function decodeToken(token) {
+	const decoded = new Promise((resolve, reject) => {
+		try {
+			const payload = jwt.verify(token, constants.JWT_SECRET);
+			resolve(payload);
+		} catch (err) {
+			reject(err);
+		}
+	});
+	return decoded;
+}
+
+module.exports = {
+	encodeToken,
+	decodeToken,
+};
---
+++ helpers/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+	authHelpers: require('./auth'),
+	responseHelpers: require('./response'),
+};
